abbec193988e5b46e44719d72ba2b4f85162c9be
Daniel Riley <dann.riley@icloud.com>
2024-03-08 18:33:25 +0100

    add about us page and Link

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/header/header";
+import styles from "./page.module.css";
+
+export default function About() {
+  return (
+    <main>
+      <Header />
+      <h1>About us!</h1>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import styles from "./page.module.css";
 import Header from "./components/header/header";
+import Section from "./components/section/section";
 import Footer from "./components/footer/footer";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Header />
+      <Section />
       <Footer />
     </main>
   );
